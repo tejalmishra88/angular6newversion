3b437cd20c8cc24fb0e9b5ed55abfa7fcd8fccaa
tejalmishra88 <tejalmishra88@gmail.com>
2018-07-24 15:43:35 +1000

    modify in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
 
-function log(target, name, descriptor ){
-  //console.log(target, name, descriptor)
-  // store the original function in a variable
-  const original = descriptor.value
-  //do some manipulation with descriptor
-  descriptor.value = function( ...args){
-    console.log("Arguments", args, "were passed in this function")
-    const result = original.apply(this,args)
-    console.log("this result of function is", result)
-    return result
-  }
-  //return the descriptor
-  return descriptor
 
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-function a(){}
+
 export class AppComponent {
   title = 'app';
 
@@ -28,7 +14,7 @@ export class AppComponent {
    console.log("this statement by constructor",this.simplemethod (5,2)) 
   }
 
-  @log
+ 
   simplemethod(a,b){
     return a*b
   }
