4217df6c01ceadf28bbe52497093644d3bc1a8c1
tejalmishra88 <tejalmishra88@gmail.com>
2018-07-26 15:16:07 +1000

    modificaton in hello.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { hellocomponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
-
+  myvariable="tejal"
+  bitwiseOR= 2 | 5
+i = 0
+somwVar= Math.random()
   constructor() { }
-
+doSomeHeavyTask(){
+  console.log(`called ${this.i++} times`)
+}
   ngOnInit() {
+    setInterval(() =>{this.someVar = Math.random()}, 50 )
   }
 
 }
