ab80294ee77da3885f855c8be70211d6b5ac5d4c
tejalmishra88 <tejalmishra88@gmail.com>
2018-08-10 14:40:15 +1000

    auth and admin component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 import { RecordsService} from './records.service';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent,
    
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule,
+    RouterModule.forRoot([
+      
+      {
+       path:'login',
+       component: LoginComponent
+      },
+      {
+        path:'admin',
+        component:AdminComponent
+      },
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ])
+
+   
+
+
   ],
   providers: [RecordsService],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+  getUserDetails(){
+    //post these details to api server return user info if correct
+  }
+}
